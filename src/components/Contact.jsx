a63f716a002ae2a1eb4d35a9dd0e07b891bffbd8
Briggitte Merchan <briggittemerchan6@gmail.com>
2022-10-12 18:26:31 -0500

    more changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contact = () => {
+
+  const [form, setForm] = useState('')
+
+  const saveForm = e => {
+    e.preventDefault();
+    setForm(e.target.contact.value)
+  }
+
+  
+
+  //console.log(form)
+
   return (
     <section className='contact section' id='contact'>
       <h2 className="section__title">Contact Me</h2>
@@ -31,26 +43,26 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <form className="contact__form grid">
+      <form onSubmit={saveForm} action="https://formsubmit.co/brimerchan@outlook.com" method="POST" className="contact__form grid">
         <div className="contact__inputs grid">
           <div className="contact__content">
             <label htmlFor="" className="contact__label">Name</label>
-            <input type="text" className="contact__input" />
+            <input type="text" id="contact" name="name" className="contact__input" />
           </div>
           <div className="contact__content">
             <label htmlFor="" className="contact__label">Email</label>
-            <input type="email" className="contact__input" />
+            <input type="email" id="contact" name="email" className="contact__input" />
           </div>
           <div className="contact__content">
             <label htmlFor="" className="contact__label">Project</label>
-            <input type="text" className="contact__input" />
+            <input type="text" id="contact" name="project" className="contact__input" />
           </div>
           <div className="contact__content">
             <label htmlFor="" className="contact__label">Message</label>
-            <textarea name="" id="" cols="0" rows="7" className='contact__input'></textarea>
+            <textarea name="message"  id="contact" cols="0" rows="7" className='contact__input'></textarea>
           </div>
           <div>
-            <button onClick={() => window.location = 'mailto:briggittemerchan6@gmail.com'} className="button button--flex">
+            <button className="button button--flex">
               Send Message
               <i className="fa-solid fa-paper-plane button__icon"></i>
             </button>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
             <p className="about__description">Innovative FrontEnd Programmer, proactive and able to develop web pages, I also have good digital skills in React.js, Javascript, HTML, CSS and Bootstrap, I have facility to work both in a team and independently, according to the needs.</p>
             <div className="about__buttons">
               <a href="https://drive.google.com/file/d/1gXPB5UmsIkYPv1NEyrSbZTmpySfMwD5-/view?usp=sharing" target="_blank" className="button button--flex">
-                See CV! <i className="fa-solid fa-down-to-line button__icon"></i>
+                View CV! <i className="fa-solid fa-down-to-line button__icon"></i>
               </a>
             </div>
           </div>
