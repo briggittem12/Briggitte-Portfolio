648ab30b3d0c4a8ef9778e08505aa697c675232c
Briggitte Merchan <briggitte>
2023-07-14 16:27:10 -0400

    delete components

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "swiper/css";
-import "swiper/css/navigation";
 import { SiMui, SiNextdotjs } from "react-icons/si";
 import { portfolio } from "../constants/portfolio";
 
