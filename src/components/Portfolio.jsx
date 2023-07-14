ae97d46a534ab9694fa1dae67d296ea1631240e8
Briggitte Merchan <briggitte>
2023-07-14 16:19:45 -0400

    section portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import { SiMui, SiNextdotjs } from "react-icons/si";
